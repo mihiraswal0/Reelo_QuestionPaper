53043d80e0afff2f526a895c571bf2da57774139
Mihir Aswal <montyaswal0@gmail.com>
2023-11-22 18:49:50 +0530

    final changes

--- server/controller.js
+++ server/controller.js
@@ -44,7 +44,9 @@ const addPaper = async (req, res) => {
     const { question, subject, topic, difficulty, marks } = req.body;
     if (!question || !subject || !topic || !difficulty || !marks)
       return res.status(404).json({ error: "Please Fill All the Details" });
-
+    const check=await QuestionStore.findOne({ question: question});
+    if(check)
+    return res.status(500).json({error:"The given question has already been added"});
     const paperAdded = await QuestionStore.create(req.body);
     res.status(200).json(paperAdded);
   } catch (err) {
